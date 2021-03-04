79f393d49c58af7bc57180194f85dd1b4af91230
Artemiy <russhun638@gmail.com>
2021-03-04 12:12:58 +0300

    Adding publisher relationship

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -26,36 +26,43 @@ public class BootStrapData implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
+        System.out.println("Started in bootstrap");
+
+        Publisher publisher = new Publisher();
+        publisher.setName("SFG publisher");
+        publisher.setCity("St Petersburg");
+        publisher.setState("FL");
+
+        publisherRepository.save(publisher);
+
         Author eric = new Author("Eric", "Evans");
         Book ddd = new Book("Domain driven design", "123123");
         eric.getBooks().add(ddd);
         ddd.getAuthors().add(eric);
 
+        ddd.setPublisher(publisher);
+        publisher.getBooks().add(ddd);
+
         authorRepository.save(eric);
         bookRepository.save(ddd);
+        publisherRepository.save(publisher);
 
         Author rod = new Author("Rod", "Johnson");
         Book noEJB = new Book("J2EE development without EJB", "123124");
         rod.getBooks().add(noEJB);
         noEJB.getAuthors().add(rod);
 
+        noEJB.setPublisher(publisher);
+        publisher.getBooks().add(noEJB);
+
+
         authorRepository.save(rod);
         bookRepository.save(noEJB);
-
-        Publisher publisher = new Publisher();
-        publisher.setName("Publisher 1");
-        publisher.setAddressLine1("Address line 1");
-        publisher.setCity("City");
-        publisher.setState("State");
-        publisher.setZip("ZipCode");
-
         publisherRepository.save(publisher);
 
-        System.out.println("Started in bootstrap");
         System.out.println("Number of books: " + bookRepository.count());
 
-        System.out.println("Publisher created: ");
-        System.out.println(publisher.toString());
+        System.out.println("Publisher number of books: " + publisher.getBooks().size());
 
     }
 
