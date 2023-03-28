8031b7b26a43f0c9d365db6825adbba0ad6e6daa
Artemiy <russhun638@gmail.com>
2023-03-28 18:16:04 +0300

    BootStrapData added

--- src/main/java/com/example/spring5webapp/domain/Book.java
+++ src/main/java/com/example/spring5webapp/domain/Book.java
@@ -1,5 +1,6 @@
 package com.example.spring5webapp.domain;
 
+import java.util.HashSet;
 import java.util.Set;
 
 import jakarta.persistence.Entity;
@@ -23,15 +24,14 @@ public class Book {
     @ManyToMany
     @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
             inverseJoinColumns = @JoinColumn(name = "author_id"))
-    private Set<Author> authors;
+    private Set<Author> authors = new HashSet<>();
 
     public Book() {
     }
 
-    public Book(String title, String isbn, Set<Author> authors) {
+    public Book(String title, String isbn) {
         this.title = title;
         this.isbn = isbn;
-        this.authors = authors;
     }
 
     public String getTitle() {
